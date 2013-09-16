a3b0ada1a90921241f87682afbc90114ce16f187
muhasjo <Tarmih-0322tm>
2013-09-16 22:27:10 +0300

    change login page

--- blog-pack/themes/diverso/views/bundles/ub/users/login.php
+++ blog-pack/themes/diverso/views/bundles/ub/users/login.php
@@ -1,10 +1,23 @@
-  <div class="hentry hentry-post group blog-big" id="leavecomment">
+<div class="hentry hentry-post">
+  <div id="respond">
     <h3>Введіть логін і пароль</h3>
-    <form action="" method="post">
-          <label for="name">Login:</label>
-          <input type="text" name="login" id="name" required class="required" >
-          <label for="email">Password:</label>
-          <input type="password" name="password" id="password" required  class="password">
-          <input type="submit" id="submit" value="Ввійти">
+
+    <form id="commentform" method="post" action="">
+        <p>
+          <label>Login</label>
+          <input type="text" aria-required="true" placeholder="your login" required name="login">
+        </p>
+
+        <p>
+          <label>Password</label>
+          <input type="password" aria-required="true" placeholder="*****" required name="password">
+
+        </p>
+        <p>
+          <button  type="submit">Ввійти</button>
+        </p>
+
     </form>
+
   </div>
+</div>
